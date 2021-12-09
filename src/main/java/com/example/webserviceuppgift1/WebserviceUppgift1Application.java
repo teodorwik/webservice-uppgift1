79375c31a056f11cfdb4511296cc9b2755f8ed60
Teodor <teodor.wik@utb.ecutbildning.se>
2021-12-09 16:18:00 +0100

    Initial commit

---
+++ src/main/java/com/example/webserviceuppgift1/WebserviceUppgift1Application.java
@@ -0,0 +1,13 @@
+package com.example.webserviceuppgift1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceUppgift1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserviceUppgift1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webserviceuppgift1/WebserviceUppgift1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webserviceuppgift1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceUppgift1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
